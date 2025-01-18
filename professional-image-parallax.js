527b660e6036e13bee05eb4db7981c42f2ba9341
sushillocal <statesu238@gmail.com>
2025-01-18 03:20:02 -0600

    Committing my first portfolio

--- professional-image-parallax.js
+++ professional-image-parallax.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
     const dp = document.querySelector('.dp');
+    
     const img = dp.querySelector('img');
     
     dp.classList.add('parallax');
